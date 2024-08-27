20092ef88a860cb911563805c037e19650b10773
Arka Dutta <52216225+ArkaDutta-Maker@users.noreply.github.com>
2024-08-28 02:31:03 +0530

    chore: Update favicon to use new image file

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "/20240602_104028.jpg",
   },
 };
 
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
